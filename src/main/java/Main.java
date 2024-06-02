17e9a4c37ffd3654c4c9ec529c59f23ad5f7efa9
andrewnnov <andrewnnov@yandex.ru>
2024-06-02 19:26:18 +0300

    added new method method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World bla bla bla");
+        System.out.println(newMethod());
+    }
+
+    private static String newMethod() {
+        return "new message";
     }
 
 }
