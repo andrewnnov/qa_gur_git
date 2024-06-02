183d778b301cf8f049a0c4324485d16a99aa624a
Andrew <andrewnnov@yandex.ru>
2024-06-02 19:25:39 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World bla bla bla");
+
+        private static String newVal = "bbbbb";
+        System.out.println(newVal);
     }
 
 }
